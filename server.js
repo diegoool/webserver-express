8184290894a7ea3e917a7f1fee6ecb65c9098d22
Diego <diego.puyol@gmail.com>
2018-10-18 13:26:42 +0200

    Primer commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+const hbs = require('hbs');
+
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+//Middleware
+app.use(express.static(__dirname + '/public'));
+
+
+// Expres HBS
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        nombre: 'diego puYol'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+
+app.listen(3000, () => {
+    console.log(`Escuchando en el puerto ${port}`);
+});
