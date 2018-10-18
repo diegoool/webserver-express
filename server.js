10a0dba12bca53212c4041caaec0fa8904873b29
Diego <diego.puyol@gmail.com>
2018-10-18 13:41:53 +0200

    4 commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 
 //Middleware
 //app.use(express.static(__dirname + '/public'));
-app.use(Express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 
 // Expres HBS
